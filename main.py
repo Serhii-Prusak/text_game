1900c66049be616f34fbcdeaaadef7c5c3442dd8
Serhii-Prusak <serhii.prusak@gmail.com>
2023-06-15 12:07:00 +0100

    first commit

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from flask import Flask, request, redirect, url_for
 # FLASK engine
 app = Flask(__name__)
 
+@app.route('/')
+def start():
+    return "You are on your path to something hidden! Are you ready?"
 if __name__ == "__main__":
     app.run()
